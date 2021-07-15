5fd7618415ecb484a079060d2c4196ab9ddd1b9c
Thiago Roberto <thiago.nemo4code@gmail.com>
2021-07-15 19:37:13 -0300

    refactor: ♻ correct definitions in ts

--- index.ts
+++ index.ts
@@ -18,16 +18,19 @@ type Car = GeoJSON.Feature<
 
 /**
  * Function that receives a position from a user,
- * the search radius in meters, a list of available cars and returns a
- * list of available cars within the search radius from the user.
+ * the search radius in meters, a list of available cars,
+ * the car model optionally and returns a list of available cars within
+ * the search radius from the user.
  *
  * @param user User
  * @param cars Car[]
- * @param radius number
+ * @param radius number (in meters)
+ * @param model number (optional)
  * @return Car[]
  */
 export const searchForNearbyCars = (
-  _user: GeoJSON.Feature,
-  _cars: GeoJSON.Feature[],
-  _radius: number
-): GeoJSON.Feature[] => [];
+  user: User,
+  cars: Car[],
+  radius: number,
+  model?: string
+): Car[] => [];
